c9ee50b81ead4bbe4882a949e89457316d980343
ValentinChanter <valentinsing19@gmail.com>
2025-05-10 12:00:56 +0200

    First commit

---
+++ run.bat
@@ -0,0 +1,106 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM ─────────────────────────────
+REM Colors (may not render in standard CMD without ANSI support)
+set "RED=[91m"
+set "BLUE=[94m"
+set "RESET=[0m"
+
+REM ─────────────────────────────
+REM Python version requirement
+set "PYTHON_REQUIRED_MAJOR=3"
+set "PYTHON_REQUIRED_MINOR=10"
+
+REM ─────────────────────────────
+REM Create venv if not exists
+if not exist "venv" (
+    REM ─────────────────────────────
+    REM CUDA prompt
+    set /p CUDA_PROMPT="Do you have a CUDA-capable GPU available? (y/n) [n]: "
+    if /i "!CUDA_PROMPT!"=="y" (
+        set "CUDA_AVAILABLE=True"
+    ) else if /i "!CUDA_PROMPT!"=="yes" (
+        set "CUDA_AVAILABLE=True"
+    ) else (
+        set "CUDA_AVAILABLE=False"
+    )
+
+    if "!CUDA_PROMPT!"=="" (
+        set "CUDA_AVAILABLE=False"
+    )
+
+    if "!CUDA_AVAILABLE!"=="True" (
+        echo Running installation for GPU.
+        set "REQUIREMENTS_FILE=requirements-gpu.txt"
+        set "TORCH_INDEX_URL=https://download.pytorch.org/whl/cu128"
+    ) else (
+        echo Running installation for CPU.
+        set "REQUIREMENTS_FILE=requirements-cpu.txt"
+    )
+
+    echo !BLUE![INFO]!RESET! Creating virtual environment...
+
+    where python >nul 2>nul
+    if errorlevel 1 (
+        echo !RED![ERROR]!RESET! Python is not installed. Please install Python and add it to your PATH.
+        exit /b 1
+    )
+
+    REM ───── Get Python version
+    for /f "delims=" %%i in ('python --version') do set "PYTHON_VERSION=%%i"
+    for /f "tokens=2" %%i in ("!PYTHON_VERSION!") do set "VERSION_NUMBER=%%i"
+    for /f "tokens=1,2,3 delims=." %%a in ("!VERSION_NUMBER!") do (
+        set "MAJOR_VERSION=%%a"
+        set "MINOR_VERSION=%%b"
+        set "PATCH_VERSION=%%c"
+    )
+
+    echo !BLUE![INFO]!RESET! Python version detected: !MAJOR_VERSION!.!MINOR_VERSION!.!PATCH_VERSION!
+
+    if !MAJOR_VERSION! LSS %PYTHON_REQUIRED_MAJOR% (
+        echo !RED![ERROR]!RESET! Python 3.10 or higher is required.
+        exit /b 1
+    )
+    if !MAJOR_VERSION!==3 if !MINOR_VERSION! LSS %PYTHON_REQUIRED_MINOR% (
+        echo !RED![ERROR]!RESET! Python 3.10 or higher is required.
+        exit /b 1
+    )
+
+    REM ───── Create virtual environment
+    python -m venv venv
+    if errorlevel 1 (
+        echo !RED![ERROR]!RESET! Failed to create virtual environment.
+        exit /b 1
+    )
+
+    REM ─────────────────────────────
+    REM Install dependencies
+    set "PYTHON_PATH=%cd%\venv\Scripts\python.exe"
+
+    echo !BLUE![INFO]!RESET! Checking if dependencies are already installed...
+    call "!PYTHON_PATH!" -c "import torch" >nul 2>nul
+    if errorlevel 1 (
+        echo !BLUE![INFO]!RESET! Installing dependencies...
+        call "!PYTHON_PATH!" -m pip install --upgrade pip
+        call "!PYTHON_PATH!" -m pip install --upgrade setuptools wheel
+
+        if "!CUDA_AVAILABLE!"=="True" (
+            REM Install PyTorch with CUDA support first, otherwise only CPU will be supported
+            call "!PYTHON_PATH!" -m pip install torch torchvision torchaudio --index-url !TORCH_INDEX_URL!
+            call "!PYTHON_PATH!" -m pip install onnxruntime-gpu
+        )
+
+        call "!PYTHON_PATH!" -m pip install -r !REQUIREMENTS_FILE!
+    )
+
+    echo !BLUE![INFO]!RESET! Dependencies installed successfully.
+) else (
+    echo !BLUE![INFO]!RESET! Virtual environment already exists. Skipping creation.
+)
+
+REM ─────────────────────────────
+REM Open a cmd with the venv activated
+echo !BLUE![INFO]!RESET! Activating virtual environment...
+call "%cd%\venv\Scripts\activate.bat"
+cmd /k
---
+++ src/index.py
@@ -0,0 +1,181 @@
+from audio_separator.separator import Separator
+import os, re, argparse
+from colorama import Fore, Style
+
+def print_info(message):
+    """
+    Print an info message in blue color.
+
+    Args:
+        message (str): The message to print.
+    """
+
+    print(f"{Fore.CYAN}[INFO] {Style.RESET_ALL}{message}")
+
+def print_warn(message):
+    """
+    Print a warning message in yellow color.
+
+    Args:
+        message (str): The message to print.
+    """
+
+    print(f"{Fore.YELLOW}[WARN] {Style.RESET_ALL}{message}")
+
+def print_error(message):
+    """
+    Print an error message in red color.
+
+    Args:
+        message (str): The message to print.
+    """
+
+    print(f"{Fore.RED}[ERROR] {Style.RESET_ALL}{message}")
+
+def get_exclusions(exclusions_file="exclusions.txt"):
+    """
+    Load exclusions from a file.
+
+    Args:
+        exclusions_file (str): The path to the exclusions file. Defaults to 'exclusions.txt'.
+    
+    Returns:
+        list: A list of file names to exclude from processing.
+    """
+
+    if os.path.exists(exclusions_file):
+        with open(exclusions_file, 'r') as file:
+            exclusions = [line.strip() for line in file.readlines()]
+
+        file.close()
+        return exclusions
+    return []
+
+def check_already_separated(folder_path):
+    folder = os.path.basename(folder_path)
+
+    matching_files = 0
+
+    for file in os.listdir(folder_path):
+        # If there's any VOC or INSTR file, some separation has already been done
+        if re.search(r"\[(?:VOC|INSTR)\]\.(?:mp3|wav|ogg)$", file):
+            matching_files += 1
+
+        if file == f"{folder}.txt":
+            with open(os.path.join(folder_path, file), 'r') as txt_file:
+                content = txt_file.read()
+                if re.search(rf"#VOCALS: ?.* \[VOC\]\.(?:mp3|wav|ogg)", content) and \
+                re.search(rf"#INSTRUMENTAL: ?.* \[INSTR\]\.(?:mp3|wav|ogg)", content):
+                    matching_files += 1
+
+            txt_file.close()
+
+    if matching_files == 3:
+        print_info(f"Already separated {folder_path}. Skipping...")
+        return True
+    else:
+        return False
+
+def separate_audio_files(songs_path, exclusions=[], force=False):
+    """
+    Separate audio files in the specified directory, excluding those in the exclusions list.
+
+    Args:
+        separator (Separator): An instance of the Separator class for audio separation.
+        songs_path (str): The path to the directory containing audio files.
+        exclusions (list): A list of file names to exclude from processing. Defaults to an empty list.
+        force (bool): If True, force re-separation of already processed files. Defaults to False.
+    """
+
+    for folder in os.listdir(songs_path):
+        if folder in exclusions:
+            print_info(f"Skipping excluded folder: {folder}")
+            continue
+
+        folder_path = os.path.join(songs_path, folder)
+
+        if not force and check_already_separated(folder_path):
+            continue
+
+        if os.path.isdir(folder_path):
+            print_info(f"Processing folder: {folder}")
+
+            mp3_name = os.path.join(folder_path, f"{folder}.mp3")
+            wav_name = os.path.join(folder_path, f"{folder}.wav")
+            ogg_name = os.path.join(folder_path, f"{folder}.ogg")
+            name = ""
+            
+            if os.path.exists(mp3_name):
+                name = mp3_name
+            elif os.path.exists(wav_name):
+                name = wav_name
+            elif os.path.exists(ogg_name):
+                name = ogg_name
+            else:
+                print_warn(f"No audio file found in {folder}. Skipping...")
+                continue
+
+            output = {
+                "Vocals": f"{folder} [VOC]",
+                "Instrumental": f"{folder} [INSTR]"
+            }
+
+            # We need to redefine the separator everytime just to specify the output directory
+            separator = Separator(output_dir=folder_path)
+            separator.load_model()
+            separator.separate(name, output)
+
+            # Add the newly created files to the .txt file only if they don't exist in the text file
+            txt_file_path = os.path.join(folder_path, f"{folder}.txt")
+            lines = []
+            if os.path.exists(txt_file_path):
+                with open(txt_file_path, 'r') as txt_file:
+                    lines = txt_file.readlines()
+
+                txt_file.close()
+
+            voc_line = f"#VOCALS: {folder} [VOC].wav\n"
+            instr_line = f"#INSTRUMENTAL: {folder} [INSTR].wav\n"
+
+            if voc_line in lines and instr_line in lines:
+                continue
+
+            insert_index = len(lines)
+            for i, line in enumerate(lines):
+                if not line.startswith("#"):
+                    insert_index = i
+                    break
+
+            if not voc_line in lines:
+                lines.insert(insert_index, voc_line)
+                insert_index += 1
+            if not instr_line in lines:
+                lines.insert(insert_index, instr_line)
+
+            with open(txt_file_path, 'w') as txt_file:
+                txt_file.writelines(lines)
+
+            txt_file.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Audio file separator script.")
+    parser.add_argument("-f", "--force", action="store_true", help="Force re-separation of already processed files.")
+    parser.add_argument("input", nargs="?", type=str, help="Specify the songs directory containing audio files.")
+    args = parser.parse_args()
+
+    force = args.force
+    if not args.input:
+        print_warn("No input directory specified. Using default: ./Songs")
+        songs_path = os.path.join(os.getcwd(), "Songs")
+    else:
+        songs_path = args.input
+        if not os.path.isabs(songs_path):
+            songs_path = os.path.abspath(songs_path)
+    
+    exclusions = get_exclusions()
+
+    if not os.path.exists(songs_path):
+        print_error(f"Directory {songs_path} does not exist.")
+        exit(1)
+    else:
+        separate_audio_files(songs_path, exclusions, force)
